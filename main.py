4da5d912a111deea45be2d287f4cd7600fbd275b
Alexandr <loginov_02>
2021-09-15 02:15:07 +0300

    Теперь работает всё, что задумывалось. Осталось совершенствовать

--- main.py
+++ main.py
@@ -7,21 +7,28 @@ import markups as nav
 from aiogram import Bot, Dispatcher, executor, types
 
 
-# Вселенная
+# --- Вселенная ---
 class World:
     def get_dict(self):
         return dict([('😂', "01"), ('🥵', "02"), ('😡', "03"), ('😰', "04"), ('🥺', "05")])
 
 
-# котики
+# --- Котики ---
 class Cats:
     def get_dict(self):
         return dict([('😂', 'aiogram_pictures/cat_1.jpg'), ('🥵', 'aiogram_pictures/cat_2.jpg'), ('😡', 'aiogram_pictures/cat_3.jpg'), ('😰', 'aiogram_pictures/cat_4.jpg'), ('🥺', 'aiogram_pictures/cat_5.jpg')])
 
-#ITMO
+# --- ITMO ---
 class Itmo:
     def get_dict(self):
         return dict([('😂', 21), ('🥵', 22), ('😡', 23), ('😰', 24), ('🥺', 25)])
+
+# --- Old But Gold ---
+class OLdButGold:
+    def get_dict(self):
+        return dict([('😂', 'aiogram_pictures/old_1.jpg'),('🤪', 'aiogram_pictures/old_2.jpg'), ('😡', 'aiogram_pictures/old_3.jpg'),
+                     ('😋', 'aiogram_pictures/old_4.jpg'), ('🤨', 'aiogram_pictures/old_5.jpg'), ('😔', 'aiogram_pictures/old_6.jpg'),
+                     ('👍', 'aiogram_pictures/old_7.jpg'), ('🖕', 'aiogram_pictures/old_8.jpg')])
 
 
 # log level
@@ -52,42 +59,61 @@ async def echo(message: types.Message):
     if x == 0:
         if "кот" in message.text.lower():
             world = Cats()
-            await message.answer("Отлично! Осталось за малым, выбирай смайлик", reply_markup=nav.emojiMenu)
+            await message.answer("Отлично! Осталось за малым, выбирай смайлик", reply_markup=nav.CatEmojiMenu)
             x = 1
         elif "итмо" in message.text.lower():
             world = Itmo()
-            await message.answer("Отлично! Осталось за малым, выбирай смайлик", reply_markup=nav.emojiMenu)
+            await message.answer("Выбрал итмо и теперь хочешь выбрать смайлик? Ха-ха, наивный\nВыбирай итмо и не выбирай вообще! Поэтому выбирай другие вселенные", reply_markup=nav.worldMenu)
+            media = types.MediaGroup()
+            media.attach_photo(types.InputFile('aiogram_pictures/itmo.jpg'))
+            await message.answer_media_group(media=media)
+        elif "old" in message.text.lower():
+            world = OLdButGold()
+            await message.answer("Хороший выбор! А теперь смайлик", reply_markup=nav.OldEmojiMenu)
             x = 1
+        elif "😔" in message.text:
+            await message.answer("Не переживай, развеселим😉. Но пока что рубрика в доработке", reply_markup=nav.worldMenu)
+            # x = 3
         else:
             await message.answer("Мне кажется, лучше следовать инструкции и выбирать вселенную из предложенных в менюшке 😉", reply_markup=nav.worldMenu)
         return
 
     if x == 1:
-        if '😂' in message.text:
+        # if '😂' in message.text:
+        #     media = types.MediaGroup()
+        #     media.attach_photo(types.InputFile(world.get_dict()['😂']))
+        #     await message.answer_media_group(media=media)
+        # elif '🥵' in message.text:
+        #     media = types.MediaGroup()
+        #     media.attach_photo(types.InputFile(world.get_dict()['🥵']))
+        #     await message.answer_media_group(media=media)
+        # elif '😡' in message.text:
+        #     media = types.MediaGroup()
+        #     media.attach_photo(types.InputFile(world.get_dict()['😡']))
+        #     await message.answer_media_group(media=media)
+        # elif '😰' in message.text:
+        #     media = types.MediaGroup()
+        #     media.attach_photo(types.InputFile(world.get_dict()['😰']))
+        #     await message.answer_media_group(media=media)
+        # elif '🥺' in message.text:
+        #     media = types.MediaGroup()
+        #     media.attach_photo(types.InputFile(world.get_dict()['🥺']))
+        #     await message.answer_media_group(media=media)
+        # elif "Выбрать вселенную" in message.text:
+        #     await message.answer("Правильно, выбери что-нибудь ещё", reply_markup=nav.worldMenu)
+        #     x = 0
+        if message.text in ['😂', '🥵', '😰', '😡', '🥺', '🤪', '😋', '🤨','😔', '👍', '🖕']:
             media = types.MediaGroup()
-            media.attach_photo(types.InputFile(world.get_dict()['😂']))
-            await message.answer_media_group(media=media)
-        elif '🥵' in message.text:
-            media = types.MediaGroup()
-            media.attach_photo(types.InputFile(world.get_dict()['🥵']))
-            await message.answer_media_group(media=media)
-        elif '😡' in message.text:
-            media = types.MediaGroup()
-            media.attach_photo(types.InputFile(world.get_dict()['😡']))
-            await message.answer_media_group(media=media)
-        elif '😰' in message.text:
-            media = types.MediaGroup()
-            media.attach_photo(types.InputFile(world.get_dict()['😰']))
-            await message.answer_media_group(media=media)
-        elif '🥺' in message.text:
-            media = types.MediaGroup()
-            media.attach_photo(types.InputFile(world.get_dict()['🥺']))
+            media.attach_photo(types.InputFile(world.get_dict()[message.text]))
             await message.answer_media_group(media=media)
         elif "Выбрать вселенную" in message.text:
             await message.answer("Правильно, выбери что-нибудь ещё", reply_markup=nav.worldMenu)
             x = 0
         else:
-            await message.answer("Извини, похоже у меня ничего нет с этим смайликом 😔. Попробуй снова, но присылай через меню", reply_markup=nav.emojiMenu)
+            await message.answer("Извини, похоже у меня ничего нет с этим смайликом 😔. Попробуй снова, но присылай через меню", reply_markup=nav.CatEmojiMenu)
+    # elif x == 2:
+    #     if message.text in ['🤪', '😋', '🤨','😔', '👍', '🖕', '😂', '😰']:
+    #
 
 
 
--- markups.py
+++ markups.py
@@ -5,12 +5,27 @@ btnMain = KeyboardButton("Выбрать вселенную")
 # --- Меню вселенных ---
 btnItmo = KeyboardButton("👨‍💻 Итмо‍")
 btnCats = KeyboardButton("😸 Котики")
-worldMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnCats, btnItmo)
+btnOld = KeyboardButton(" Old_But_Gold")
+btnFun = KeyboardButton("Грущу, просто развеселите 😔")
+worldMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnCats, btnItmo, btnOld, btnFun)
 
-# --- Меню смайлов ---
-btnEmoji1 = KeyboardButton("😂")
-btnEmoji2 = KeyboardButton("🥵")
-btnEmoji3 = KeyboardButton("😡")
-btnEmoji4 = KeyboardButton("😰")
-btnEmoji5 = KeyboardButton("🥺")
-emojiMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnEmoji5, btnEmoji4, btnEmoji3, btnEmoji2, btnEmoji1, btnMain)
+# --- Меню смайлов котиков ---
+btnCatEmoji1 = KeyboardButton("😂")
+btnCatEmoji2 = KeyboardButton("🥵")
+btnCatEmoji3 = KeyboardButton("😡")
+btnCatEmoji4 = KeyboardButton("😰")
+btnCatEmoji5 = KeyboardButton("🥺")
+CatEmojiMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnCatEmoji5, btnCatEmoji4, btnCatEmoji3,
+                                                             btnCatEmoji2, btnCatEmoji1, btnMain)
+
+# --- Меню смайлов олдов ---
+btnOldEmoji1 = KeyboardButton('🖕')
+btnOldEmoji2 = KeyboardButton('👍')
+btnOldEmoji3 = KeyboardButton('😔')
+btnOldEmoji4 = KeyboardButton('🤨')
+btnOldEmoji5 = KeyboardButton('😋')
+btnOldEmoji6 = KeyboardButton('🤪')
+btnOldEmoji7 = KeyboardButton('😂')
+btnOldEmoji8 = KeyboardButton('😡')
+OldEmojiMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnOldEmoji8, btnOldEmoji7, btnOldEmoji6, btnOldEmoji5, btnOldEmoji4,
+                                                             btnOldEmoji3, btnOldEmoji2, btnOldEmoji1, btnMain)
